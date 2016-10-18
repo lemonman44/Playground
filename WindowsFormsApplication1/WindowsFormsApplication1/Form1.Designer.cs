eaf42d4e3bc01e67dc339160c713bf5c71cd1c15
lemonman44 <AdamLehman2018@gmail.com>
2016-10-18 14:07:12 -0500

    filled in code

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.Panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.Panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.button1.Location = new System.Drawing.Point(580, 488);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(245, 136);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "gdsfhgwrthw\r\n";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(580, 663);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(245, 125);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(0, 0);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 75);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Back";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Panel1
+            // 
+            this.Panel1.Controls.Add(this.button4);
+            this.Panel1.Controls.Add(this.button3);
+            this.Panel1.Controls.Add(this.button2);
+            this.Panel1.Controls.Add(this.button1);
+            this.Panel1.Location = new System.Drawing.Point(-1400, 0);
+            this.Panel1.Name = "Panel1";
+            this.Panel1.Size = new System.Drawing.Size(4200, 4200);
+            this.Panel1.TabIndex = 0;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(2800, 0);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 75);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "back2";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1374, 1329);
+            this.Controls.Add(this.Panel1);
+            this.MinimumSize = new System.Drawing.Size(800, 800);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.Panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Panel Panel1;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        private int pagePosition = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pagePosition = 0;
+            button3.Visible = true;
+            Panel1.Location = new Point(Panel1.Location.X + ClientSize.Width, 0);
+            button1.Visible = false;
+            button2.Visible = false;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pagePosition = 2;
+            button3.Visible = true;
+            Panel1.Location = new Point(Panel1.Location.X - ClientSize.Width, 0);
+            button1.Visible = false;
+            button2.Visible = false;
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pagePosition = 1;
+            button3.Visible = false;
+            Panel1.Location = new Point(Panel1.Location.X - ClientSize.Width, 0);
+            button1.Visible = true;
+            button2.Visible = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pagePosition = 1;
+            button3.Visible = false;
+            Panel1.Location = new Point(Panel1.Location.X + ClientSize.Width, 0);
+            button1.Visible = true;
+            button2.Visible = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int button1_X = ClientSize.Width / 2 + ClientSize.Width - button1.Width / 2;
+            int button2_X = ClientSize.Width / 2 + ClientSize.Width - button2.Width / 2;
+            int button1_Y = ClientSize.Height / 2 + button1.Height;
+            int button2_Y = ClientSize.Height / 2 - button2.Height;
+
+            Panel1.Size = new Size(ClientSize.Width * 3, ClientSize.Height);
+            Panel1.Location = new Point(-ClientSize.Width * pagePosition, 0);
+            button1.Location = new Point(button1_X, button1_Y);
+            button2.Location = new Point(button2_X, button2_Y);
+            button4.Location = new Point(ClientSize.Width * 2, 0);
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            int button1_X = ClientSize.Width / 2 + ClientSize.Width - button1.Width / 2;
+            int button2_X = ClientSize.Width / 2 + ClientSize.Width - button2.Width / 2;
+            int button1_Y = ClientSize.Height / 2 + button1.Height;
+            int button2_Y = ClientSize.Height / 2 - button2.Height;
+
+            Panel1.Size = new Size(ClientSize.Width * 3, ClientSize.Height);
+            Panel1.Location = new Point(-ClientSize.Width * pagePosition, 0);
+            button1.Location = new Point(button1_X, button1_Y);
+            button2.Location = new Point(button2_X, button2_Y);
+            button4.Location = new Point(ClientSize.Width * 2, 0);
+        }
+    }
+}
