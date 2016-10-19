9f79ffd02b195cf4109f62ac19f577d3e02484db
Brendon Brown <bbrown16@northlandcaps.org>
2016-10-19 14:24:47 -0500

    Made a class trhat makes a 2 dimensional array and fills it with numbers from 1-100

--- WindowsFormsApplication1/WindowsFormsApplication1/Button.cs
+++ WindowsFormsApplication1/WindowsFormsApplication1/Button.cs
@@ -8,5 +8,21 @@ namespace WindowsFormsApplication1
 {
     class Button
     {
+        static void Main()
+        {
+            Random random = new Random();
+            int[,] number_Storage = new int[100, 100];
+            int y = 100;
+            int x = 100;
+            while (y != 0)
+            {
+                while (x != 0)
+                {
+                    number_Storage[x-1, y-1] = random.Next(1, 100);
+                    x--;
+                }
+                y--;
+            }
+        }
     }
 }
