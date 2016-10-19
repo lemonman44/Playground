30100cc4107171fe678d01867df1d39111cdf4f8
lemonman44 <AdamLehman2018@gmail.com>
2016-10-19 14:30:33 -0500

    new class to process the array

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Process.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication1
+{
+    class Process
+    {
+        int[,] correctArray = new int[100, 100];
+        private void setCorrectArray(int[,] correctArray)
+        {
+            
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+
+                }
+            }
+        }
+        private object getCorrectArray()
+        {
+            return correctArray;
+        }
+    }
+}
